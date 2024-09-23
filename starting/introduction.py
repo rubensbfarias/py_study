298b9faf55cdc640390c98ec6e2aa8f6253c2e6e
Rubens Batista de Farias <rubens.farias@inorpelcybersecurity.com.br>
2024-09-23 10:17:39 -0300

    create: study guide

---
+++ starting/introduction.py
@@ -0,0 +1,13 @@
+
+# receiving data of users
+
+# data entry
+name = input("What's your name? ")
+age = input("How old are you? ")
+year = input("What year is it? ")
+
+# processing
+
+# data output
+print(f'Welcome to the program {name} your age is {age}')
+print(f'{name} Year of birth {int(year) - int(age)}')
